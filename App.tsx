1ecc40d9c0ff0044f9e72494dd146b227299162a
marceloglacial <glacial@gmail.com>
2024-01-26 23:15:15 +0000

    fix tunnel and reset

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import React = require("react");
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 bg-white items-center justify-center">
-      <View className="items-center justify-center bg-red-600 p-8 rounded-lg">
-        <Text className="text-2xl text-white text-center">
-          Open up App.tsx to start working on your app!
-        </Text>
-        <StatusBar style="auto" />
-      </View>
-    </SafeAreaView>
+    <View className="flex-1 items-center justify-center bg-red-600">
+      <Text className="text-white text-4xl text-center">
+        Open up App.tsx to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
